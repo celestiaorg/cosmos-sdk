fa29549576134558a980d48de8e4df72e8aa3de4
rachid <chamirachid1@gmail.com>
2022-05-09 13:52:45 +0200

    increase DefaultGenTxGas

--- simapp/helpers/test_helpers.go
+++ simapp/helpers/test_helpers.go
@@ -14,7 +14,7 @@ import (
 
 // SimAppChainID hardcoded chainID for simulation
 const (
-	DefaultGenTxGas = 2100000 // An increase is needed when adding checks to staking/msg_server.go
+	DefaultGenTxGas = 2500000 // An increase is needed when adding checks to staking/msg_server.go
 	SimAppChainID   = "simulation-app"
 )
 

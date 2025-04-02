57d03951271f9bd4be528604acf361e897e458c5
Damian Nolan <damiannolan@gmail.com>
2025-04-02 14:33:11 +0200

    chore: add timer to x/upgrade for benchmarking

--- x/upgrade/keeper/keeper.go
+++ x/upgrade/keeper/keeper.go
@@ -10,6 +10,7 @@ import (
 	"path/filepath"
 	"sort"
 	"strconv"
+	"time"
 
 	"github.com/hashicorp/go-metrics"
 
@@ -44,6 +45,8 @@ type Keeper struct {
 	downgradeVerified  bool                            // tells if we've already sanity checked that this binary version isn't being used against an old state.
 	authority          string                          // the address capable of executing and canceling an upgrade. Usually the gov module account
 	initVersionMap     module.VersionMap               // the module version map at init genesis
+
+	UpgradeTime time.Time
 }
 
 // NewKeeper constructs an upgrade Keeper which requires the following arguments:

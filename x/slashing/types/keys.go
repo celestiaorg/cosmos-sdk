dd6c24bf622820f3d0d00f14c059f503bf0b057e
Damian Nolan <damiannolan@gmail.com>
2025-04-04 13:31:57 +0200

    chore: update doc string in comments

--- x/slashing/types/keys.go
+++ x/slashing/types/keys.go
@@ -43,7 +43,7 @@ const (
 //
 // - 0x01<consAddrLen (1 Byte)><consAddress_Bytes>: ValidatorSigningInfo
 //
-// - 0x02<consAddrLen (1 Byte)><consAddress_Bytes><chunk_index>: bitmap_chunk
+// - 0x12<consAddrLen (1 Byte)><consAddress_Bytes><chunk_index>: bitmap_chunk
 //
 // - 0x03<accAddrLen (1 Byte)><accAddr_Bytes>: cryptotypes.PubKey
 

aa1679d7cf159487b407f85bfd3ee616be5b1e63
chatton <github.qpeyb@simplelogin.fr>
2025-04-09 15:00:57 +0100

    chore: pr feedback

--- server/grpc/server.go
+++ server/grpc/server.go
@@ -20,7 +20,7 @@ import (
 )
 
 // StartGRPCServer starts a gRPC server on the given address.
-func StartGRPCServer(clientCtx client.Context, app types.Application, cfg config.GRPCConfig, withTm bool) (*grpc.Server, error) {
+func StartGRPCServer(clientCtx client.Context, app types.Application, cfg config.GRPCConfig, enableCoreGRPC bool) (*grpc.Server, error) {
 	maxSendMsgSize := cfg.MaxSendMsgSize
 	if maxSendMsgSize == 0 {
 		maxSendMsgSize = config.DefaultGRPCMaxSendMsgSize
@@ -37,7 +37,7 @@ func StartGRPCServer(clientCtx client.Context, app types.Application, cfg config
 		grpc.MaxRecvMsgSize(maxRecvMsgSize),
 	)
 
-	if withTm {
+	if enableCoreGRPC {
 		// start the gRPC block API only if running alongside an in-process tendermint node.
 		api := coregrpc.NewBlockAPI()
 		go api.StartNewBlockEventListener(context.Background())

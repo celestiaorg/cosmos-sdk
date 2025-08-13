bedd6d1f97b6ee70a05d73fb7d1210d01a07b226
evan-forbes <evan.samuel.forbes@gmail.com>
2025-08-13 12:07:55 -0500

    chore: use the priority mempool in network tests

--- testutil/network/network.go
+++ testutil/network/network.go
@@ -368,6 +368,7 @@ func New(l Logger, baseDir string, cfg Config) (*Network, error) {
 
 		ctx := server.NewDefaultContext()
 		cmtCfg := ctx.Config
+		cmtCfg.Mempool.Type = "priority"
 		cmtCfg.Consensus.TimeoutCommit = cfg.TimeoutCommit
 
 		// Only allow the first validator to expose an RPC, API and gRPC

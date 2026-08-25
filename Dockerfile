celestiaorg/cosmos-sdk

commit 0f1f946be67e8a02a9615bfcd9e68716f48760e3

fix: use new protos to fix comet's serialization (#642)
* fix: use new protos to fix comet's serialization

* chore: bump

* chore: delete exampmle tests as we don't care about those

* chore: dgaf about these examples bruh

* chore: bump to official release version

types/events.go
@@ -98,8 +98,8 @@ func TypedEventToEvent(tev proto.Message) (Event, error) {
 	for _, k := range keys {
 		v := attrMap[k]
 		attrs = append(attrs, abci.EventAttribute{
-			Key:   []byte(k),
-			Value: v,
+			Key:   k,
+			Value: string(v),
 		})
 	}
 
@@ -186,7 +186,7 @@ func (a Attribute) String() string {
 
 // ToKVPair converts an Attribute object into a Tendermint key/value pair.
 func (a Attribute) ToKVPair() abci.EventAttribute {
-	return abci.EventAttribute{Key: toBytes(a.Key), Value: toBytes(a.Value)}
+	return abci.EventAttribute{Key: a.Key, Value: a.Value}
 }
 
 func toBytes(i interface{}) []byte {

crypto/hd/hdpath_test.go
@@ -1,7 +1,6 @@
 package hd_test
 
 import (
-	"encoding/hex"
 	"fmt"
 	"testing"
 
@@ -183,102 +182,6 @@ func TestDeriveHDPathRange(t *testing.T) {
 			}
 		})
 	}
-}
-
-func ExampleStringifyPathParams() {
-	path := hd.NewParams(44, 0, 0, false, 0)
-	fmt.Println(path.String())
-	path = hd.NewParams(44, 33, 7, true, 9)
-	fmt.Println(path.String())
-	// Output:
-	// m/44'/0'/0'/0/0
-	// m/44'/33'/7'/1/9
-}
-
-func ExampleSomeBIP32TestVecs() {
-	seed := mnemonicToSeed("barrel original fuel morning among eternal " +
-		"filter ball stove pluck matrix mechanic")
-	master, ch := hd.ComputeMastersFromSeed(seed)
-	fmt.Println("keys from fundraiser test-vector (cosmos, bitcoin, ether)")
-	fmt.Println()
-	// cosmos
-	priv, err := hd.DerivePrivateKeyForPath(master, ch, types.FullFundraiserPath)
-	if err != nil {
-		fmt.Println("INVALID")
-	} else {
-		fmt.Println(hex.EncodeToString(priv[:]))
-	}
-	// bitcoin
-	priv, err = hd.DerivePrivateKeyForPath(master, ch, "44'/0'/0'/0/0")
-	if err != nil {
-		fmt.Println("INVALID")
-	} else {
-		fmt.Println(hex.EncodeToString(priv[:]))
-	}
-	// ether
-	priv, err = hd.DerivePrivateKeyForPath(master, ch, "44'/60'/0'/0/0")
-	if err != nil {
-		fmt.Println("INVALID")
-	} else {
-		fmt.Println(hex.EncodeToString(priv[:]))
-	}
-	// INVALID
-	priv, err = hd.DerivePrivateKeyForPath(master, ch, "X/0'/0'/0/0")
-	if err != nil {
-		fmt.Println("INVALID")
-	} else {
-		fmt.Println(hex.EncodeToString(priv[:]))
-	}
-	priv, err = hd.DerivePrivateKeyForPath(master, ch, "-44/0'/0'/0/0")
-	if err != nil {
-		fmt.Println("INVALID")
-	} else {
-		fmt.Println(hex.EncodeToString(priv[:]))
-	}
-
-	fmt.Println()
-	fmt.Println("keys generated via https://coinomi.com/recovery-phrase-tool.html")
-	fmt.Println()
-
-	seed = mnemonicToSeed(
-		"advice process birth april short trust crater change bacon monkey medal garment " +
-			"gorilla ranch hour rival razor call lunar mention taste vacant woman sister")
-	master, ch = hd.ComputeMastersFromSeed(seed)
-	priv, _ = hd.DerivePrivateKeyForPath(master, ch, "44'/1'/1'/0/4")
-	fmt.Println(hex.EncodeToString(priv[:]))
-
-	seed = mnemonicToSeed("idea naive region square margin day captain habit " +
-		"gun second farm pact pulse someone armed")
-	master, ch = hd.ComputeMastersFromSeed(seed)
-	priv, _ = hd.DerivePrivateKeyForPath(master, ch, "44'/0'/0'/0/420")
-	fmt.Println(hex.EncodeToString(priv[:]))
-
-	fmt.Println()
-	fmt.Println("BIP 32 example")
-	fmt.Println()
-
-	// bip32 path: m/0/7
-	seed = mnemonicToSeed("monitor flock loyal sick object grunt duty ride develop assault harsh history")
-	master, ch = hd.ComputeMastersFromSeed(seed)
-	priv, _ = hd.DerivePrivateKeyForPath(master, ch, "0/7")
-	fmt.Println(hex.EncodeToString(priv[:]))
-
-	// Output: keys from fundraiser test-vector (cosmos, bitcoin, ether)
-	//
-	// bfcb217c058d8bbafd5e186eae936106ca3e943889b0b4a093ae13822fd3170c
-	// e77c3de76965ad89997451de97b95bb65ede23a6bf185a55d80363d92ee37c3d
-	// 7fc4d8a8146dea344ba04c593517d3f377fa6cded36cd55aee0a0bb968e651bc
-	// INVALID
-	// INVALID
-	//
-	// keys generated via https://coinomi.com/recovery-phrase-tool.html
-	//
-	// a61f10c5fecf40c084c94fa54273b6f5d7989386be4a37669e6d6f7b0169c163
-	// 32c4599843de3ef161a629a461d12c60b009b676c35050be5f7ded3a3b23501f
-	//
-	// BIP 32 example
-	//
-	// c4c11d8c03625515905d7e89d25dfc66126fbc629ecca6db489a1a72fc4bda78
 }
 
 // Ensuring that we don't crash if values have trailing slashes

crypto/ledger/encode_test.go
@@ -1,7 +1,6 @@
 package ledger
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -26,21 +25,6 @@ func checkAminoJSON(t *testing.T, src interface{}, dst interface{}, isNil bool) 
 	// Unmarshal.
 	err = cdc.UnmarshalJSON(js, dst)
 	require.Nil(t, err, "%+v", err)
-}
-
-// nolint: govet
-func ExamplePrintRegisteredTypes() {
-	_ = cdc.PrintTypes(os.Stdout)
-	// | Type | Name | Prefix | Length | Notes |
-	// | ---- | ---- | ------ | ----- | ------ |
-	// | PrivKeyLedgerSecp256k1 | tendermint/PrivKeyLedgerSecp256k1 | 0x10CAB393 | variable |  |
-	// | PubKey | tendermint/PubKeyEd25519 | 0x1624DE64 | variable |  |
-	// | PubKey | tendermint/PubKeySr25519 | 0x0DFB1005 | variable |  |
-	// | PubKey | tendermint/PubKeySecp256k1 | 0xEB5AE987 | variable |  |
-	// | PubKeyMultisigThreshold | tendermint/PubKeyMultisigThreshold | 0x22C1F7E2 | variable |  |
-	// | PrivKey | tendermint/PrivKeyEd25519 | 0xA3288910 | variable |  |
-	// | PrivKey | tendermint/PrivKeySr25519 | 0x2F82D78B | variable |  |
-	// | PrivKey | tendermint/PrivKeySecp256k1 | 0xE1B0F79B | variable |  |
 }
 
 func TestNilEncodings(t *testing.T) {

go.mod
@@ -1,4 +1,6 @@
-go 1.23.5
+go 1.23.6
+
+toolchain go1.24.4
 
 module github.com/cosmos/cosmos-sdk
 
@@ -37,8 +39,8 @@ require (
 	github.com/manifoldco/promptui v0.9.0
 	github.com/mattn/go-isatty v0.0.18
 	github.com/pkg/errors v0.9.1
-	github.com/prometheus/client_golang v1.20.3
-	github.com/prometheus/common v0.55.0
+	github.com/prometheus/client_golang v1.20.5
+	github.com/prometheus/common v0.62.0
 	github.com/rakyll/statik v0.1.7
 	github.com/regen-network/cosmos-proto v0.3.1
 	github.com/rs/zerolog v1.29.1
@@ -51,11 +53,11 @@ require (
 	github.com/tendermint/tendermint v0.34.28
 	github.com/tendermint/tm-db v0.6.7
 	github.com/tidwall/btree v1.5.0
-	golang.org/x/crypto v0.31.0
-	golang.org/x/exp v0.0.0-20240904232852-e7e105dedf7e
+	golang.org/x/crypto v0.35.0
+	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8
 	google.golang.org/genproto/googleapis/api v0.0.0-20240604185151-ef581f913117
 	google.golang.org/grpc v1.66.0
-	google.golang.org/protobuf v1.34.2
+	google.golang.org/protobuf v1.36.4
 	pgregory.net/rapid v0.4.7
 	sigs.k8s.io/yaml v1.3.0
 )
@@ -68,6 +70,7 @@ require (
 	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
 	github.com/99designs/go-keychain v0.0.0-20191008050251-8e49817e8af4 // indirect
 	github.com/ChainSafe/go-schnorrkel v1.0.0 // indirect
+	github.com/DataDog/zstd v1.5.6 // indirect
 	github.com/Workiva/go-datastructures v1.0.53 // indirect
 	github.com/aws/aws-sdk-go v1.40.45 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -76,29 +79,38 @@ require (
 	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/cometbft/cometbft-db v0.7.0 // indirect
+	github.com/cockroachdb/errors v1.11.3 // indirect
+	github.com/cockroachdb/fifo v0.0.0-20240816210425-c5d0cb0b6fc0 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20241215232642-bb51bb14a506 // indirect
+	github.com/cockroachdb/pebble v1.1.4 // indirect
+	github.com/cockroachdb/redact v1.1.5 // indirect
+	github.com/cockroachdb/tokenbucket v0.0.0-20230807174530-cc333fc44b06 // indirect
+	github.com/cometbft/cometbft-db v1.0.3 // indirect
 	github.com/cosmos/gorocksdb v1.2.0 // indirect
 	github.com/danieljoos/wincred v1.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
 	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/dgraph-io/badger/v2 v2.2007.4 // indirect
-	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dgraph-io/badger/v4 v4.5.1 // indirect
+	github.com/dgraph-io/ristretto v1.0.0 // indirect
+	github.com/dgraph-io/ristretto/v2 v2.1.0 // indirect
 	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/dvsekhvalnov/jose2go v1.5.0 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/getsentry/sentry-go v0.31.1 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
-	github.com/golang/glog v1.2.1 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/btree v1.1.2 // indirect
+	github.com/google/btree v1.1.3 // indirect
+	github.com/google/flatbuffers v25.1.24+incompatible // indirect
 	github.com/google/orderedcode v0.0.1 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
@@ -118,9 +130,12 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/jmhodges/levigo v1.0.0 // indirect
-	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
+	github.com/linxGnu/grocksdb v1.9.8 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mimoo/StrobeGo v0.0.0-20210601165009-122bf33a46e0 // indirect
 	github.com/minio/highwayhash v1.0.3 // indirect
@@ -135,29 +150,29 @@ require (
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/rs/cors v1.8.3 // indirect
 	github.com/sasha-s/go-deadlock v0.3.1 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/syndtr/goleveldb v1.0.1-0.20220721030215-126854af5e6d // indirect
-	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
 	github.com/ulikunitz/xz v0.5.8 // indirect
 	github.com/zondax/hid v0.9.2 // indirect
 	github.com/zondax/ledger-go v0.14.3 // indirect
-	go.etcd.io/bbolt v1.3.6 // indirect
+	go.etcd.io/bbolt v1.3.11 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/otel v1.30.0 // indirect
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.18.0 // indirect
 	go.opentelemetry.io/otel/metric v1.30.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.30.0 // indirect
 	go.opentelemetry.io/otel/trace v1.30.0 // indirect
-	golang.org/x/net v0.33.0 // indirect
-	golang.org/x/oauth2 v0.21.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/term v0.27.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/oauth2 v0.24.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/api v0.152.0 // indirect
@@ -183,7 +198,7 @@ replace (
 	// replace broken goleveldb.
 	github.com/syndtr/goleveldb => github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7
 	// use cometbft
-	github.com/tendermint/tendermint => github.com/celestiaorg/celestia-core v1.49.0-tm-v0.34.35
+	github.com/tendermint/tendermint => github.com/celestiaorg/celestia-core v1.55.0-tm-v0.34.35
 )
 
 retract (

types/events_test.go
@@ -158,15 +158,15 @@ func (s *eventsTestSuite) TestMarkEventsToIndex() {
 		{
 			Type: "message",
 			Attributes: []abci.EventAttribute{
-				{Key: []byte("sender"), Value: []byte("foo")},
-				{Key: []byte("recipient"), Value: []byte("bar")},
+				{Key: "sender", Value: "foo"},
+				{Key: "recipient", Value: "bar"},
 			},
 		},
 		{
 			Type: "staking",
 			Attributes: []abci.EventAttribute{
-				{Key: []byte("deposit"), Value: []byte("5")},
-				{Key: []byte("unbond"), Value: []byte("10")},
+				{Key: "deposit", Value: "5"},
+				{Key: "unbond", Value: "10"},
 			},
 		},
 	}
@@ -182,15 +182,15 @@ func (s *eventsTestSuite) TestMarkEventsToIndex() {
 				{
 					Type: "message",
 					Attributes: []abci.EventAttribute{
-						{Key: []byte("sender"), Value: []byte("foo"), Index: true},
-						{Key: []byte("recipient"), Value: []byte("bar"), Index: true},
+						{Key: "sender", Value: "foo", Index: true},
+						{Key: "recipient", Value: "bar", Index: true},
 					},
 				},
 				{
 					Type: "staking",
 					Attributes: []abci.EventAttribute{
-						{Key: []byte("deposit"), Value: []byte("5"), Index: true},
-						{Key: []byte("unbond"), Value: []byte("10"), Index: true},
+						{Key: "deposit", Value: "5", Index: true},
+						{Key: "unbond", Value: "10", Index: true},
 					},
 				},
 			},
@@ -202,15 +202,15 @@ func (s *eventsTestSuite) TestMarkEventsToIndex() {
 				{
 					Type: "message",
 					Attributes: []abci.EventAttribute{
-						{Key: []byte("sender"), Value: []byte("foo"), Index: true},
-						{Key: []byte("recipient"), Value: []byte("bar")},
+						{Key: "sender", Value: "foo", Index: true},
+						{Key: "recipient", Value: "bar"},
 					},
 				},
 				{
 					Type: "staking",
 					Attributes: []abci.EventAttribute{
-						{Key: []byte("deposit"), Value: []byte("5"), Index: true},
-						{Key: []byte("unbond"), Value: []byte("10")},
+						{Key: "deposit", Value: "5", Index: true},
+						{Key: "unbond", Value: "10"},
 					},
 				},
 			},
@@ -225,15 +225,15 @@ func (s *eventsTestSuite) TestMarkEventsToIndex() {
 				{
 					Type: "message",
 					Attributes: []abci.EventAttribute{
-						{Key: []byte("sender"), Value: []byte("foo"), Index: true},
-						{Key: []byte("recipient"), Value: []byte("bar"), Index: true},
+						{Key: "sender", Value: "foo", Index: true},
+						{Key: "recipient", Value: "bar", Index: true},
 					},
 				},
 				{
 					Type: "staking",
 					Attributes: []abci.EventAttribute{
-						{Key: []byte("deposit"), Value: []byte("5"), Index: true},
-						{Key: []byte("unbond"), Value: []byte("10"), Index: true},
+						{Key: "deposit", Value: "5", Index: true},
+						{Key: "unbond", Value: "10", Index: true},
 					},
 				},
 			},

types/query/filtered_pagination_test.go
@@ -2,7 +2,6 @@ package query_test
 
 import (
 	"fmt"
-	"testing"
 
 	"cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -169,61 +168,6 @@ func (s *paginationTestSuite) TestReverseFilteredPaginations() {
 
 	s.T().Log("verify Reverse pagination returns valid result")
 	s.Require().Equal(balances[235:241].String(), balns.Sort().String())
-}
-
-func ExampleFilteredPaginate(t *testing.T) {
-	app, ctx, _ := setupTest(t)
-
-	var balances sdk.Coins
-	for i := 0; i < numBalances; i++ {
-		denom := fmt.Sprintf("foo%ddenom", i)
-		balances = append(balances, sdk.NewInt64Coin(denom, 100))
-	}
-
-	for i := 0; i < 5; i++ {
-		denom := fmt.Sprintf("test%ddenom", i)
-		balances = append(balances, sdk.NewInt64Coin(denom, 250))
-	}
-
-	balances = balances.Sort()
-	addr1 := sdk.AccAddress([]byte("addr1"))
-	acc1 := app.AccountKeeper.NewAccountWithAddress(ctx, addr1)
-	app.AccountKeeper.SetAccount(ctx, acc1)
-	err := testutil.FundAccount(app.BankKeeper, ctx, addr1, balances)
-	if err != nil { // should return no error
-		fmt.Println(err)
-	}
-
-	pageReq := &query.PageRequest{Key: nil, Limit: 1, CountTotal: true}
-	store := ctx.KVStore(app.GetKey(types.StoreKey))
-	balancesStore := prefix.NewStore(store, types.BalancesPrefix)
-	accountStore := prefix.NewStore(balancesStore, address.MustLengthPrefix(addr1))
-
-	var balResult sdk.Coins
-	pageRes, err := query.FilteredPaginate(accountStore, pageReq, func(key []byte, value []byte, accumulate bool) (bool, error) {
-		var amount math.Int
-		err := amount.Unmarshal(value)
-		if err != nil {
-			return false, err
-		}
-
-		// filter amount greater than 100
-		if amount.Int64() > int64(100) {
-			if accumulate {
-				balResult = append(balResult, sdk.NewCoin(string(key), amount))
-			}
-
-			return true, nil
-		}
-
-		return false, nil
-	})
-	if err != nil { // should return no error
-		fmt.Println(err)
-	}
-	fmt.Println(&types.QueryAllBalancesResponse{Balances: balResult, Pagination: pageRes})
-	// Output:
-	// balances:<denom:"test0denom" amount:"250" > pagination:<next_key:"test1denom" total:5 >
 }
 
 func execFilterPaginate(store sdk.KVStore, pageReq *query.PageRequest, appCodec codec.Codec) (balances sdk.Coins, res *query.PageResponse, err error) {

types/query/pagination_test.go
@@ -5,19 +5,14 @@ import (
 	"fmt"
 	"testing"
 
-	"cosmossdk.io/math"
 	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-	dbm "github.com/tendermint/tm-db"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
 	"github.com/cosmos/cosmos-sdk/simapp"
-	"github.com/cosmos/cosmos-sdk/store"
-	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/address"
 	"github.com/cosmos/cosmos-sdk/types/query"
 	"github.com/cosmos/cosmos-sdk/x/bank/testutil"
 	"github.com/cosmos/cosmos-sdk/x/bank/types"
@@ -294,57 +289,10 @@ func (s *paginationTestSuite) TestReversePagination() {
 	s.Require().Nil(res.Pagination.NextKey)
 }
 
-func ExamplePaginate(t *testing.T) {
-	app, ctx, _ := setupTest(t)
-
-	var balances sdk.Coins
-
-	for i := 0; i < 2; i++ {
-		denom := fmt.Sprintf("foo%ddenom", i)
-		balances = append(balances, sdk.NewInt64Coin(denom, 100))
-	}
-
-	balances = balances.Sort()
-	addr1 := sdk.AccAddress([]byte("addr1"))
-	acc1 := app.AccountKeeper.NewAccountWithAddress(ctx, addr1)
-	app.AccountKeeper.SetAccount(ctx, acc1)
-	err := testutil.FundAccount(app.BankKeeper, ctx, addr1, balances)
-	if err != nil { // should return no error
-		fmt.Println(err)
-	}
-	// Paginate example
-	pageReq := &query.PageRequest{Key: nil, Limit: 1, CountTotal: true}
-	request := types.NewQueryAllBalancesRequest(addr1, pageReq)
-	balResult := sdk.NewCoins()
-	authStore := ctx.KVStore(app.GetKey(types.StoreKey))
-	balancesStore := prefix.NewStore(authStore, types.BalancesPrefix)
-	accountStore := prefix.NewStore(balancesStore, address.MustLengthPrefix(addr1))
-	pageRes, err := query.Paginate(accountStore, request.Pagination, func(key []byte, value []byte) error {
-		var amount math.Int
-		err := amount.Unmarshal(value)
-		if err != nil {
-			return err
-		}
-		balResult = append(balResult, sdk.NewCoin(string(key), amount))
-		return nil
-	})
-	if err != nil { // should return no error
-		fmt.Println(err)
-	}
-	fmt.Println(&types.QueryAllBalancesResponse{Balances: balResult, Pagination: pageRes})
-	// Output:
-	// balances:<denom:"foo0denom" amount:"100" > pagination:<next_key:"foo1denom" total:2 >
-}
-
 func setupTest(t *testing.T) (*simapp.SimApp, sdk.Context, codec.Codec) {
 	app := simapp.Setup(t, false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{Height: 1})
 	appCodec := app.AppCodec()
-
-	db := dbm.NewMemDB()
-	ms := store.NewCommitMultiStore(db)
-
-	ms.LoadLatestVersion()
 
 	return app, ctx, appCodec
 }

commit b568172e09f89781a87c1e5587540c70533e06dc

ci: fix failing lint, docker build, and liveness workflows
- Bump golang base image 1.22.1-alpine -> 1.24-alpine in the simapp,
  simd-env, and simd-dlv Dockerfiles: go.mod requires go >= 1.23.6 and
  the official golang images pin GOTOOLCHAIN=local, so builds failed.
- Use `docker compose` (v2 plugin) in the localnet Makefile targets:
  ubuntu-24.04 runners no longer ship the standalone docker-compose.
- Bump golangci-lint v1.61.0 -> v1.64.8: v1.61.0 cannot read Go 1.24
  export data ("unsupported version: 2" typecheck errors) now that
  go.mod declares toolchain go1.24.4.
- Drop the exportloopref linter (fully inactivated in v1.64) and fix
  the unconvert/unused findings v1.61.0 never got far enough to report
  (fallout from #642 changing event attributes from []byte to string).

Closes #589

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01VYUn5iYqjJzxx8pKj1MUXp

.github/workflows/lint.yml
@@ -36,4 +36,4 @@ jobs:
         if: env.GIT_DIFF
         uses: golangci/golangci-lint-action@v6.1.0
         with:
-          version: v1.61.0
+          version: v1.64.8

.golangci.yml
@@ -7,7 +7,6 @@ linters:
   disable-all: true
   enable:
     - dogsled
-    - exportloopref
     - goconst
     - gocritic
     - gofumpt

Dockerfile
@@ -10,7 +10,7 @@
 # > docker run -it -p 26657:26657 -p 26656:26656 -v ~/.simappcli:/root/.simapp simapp simd keys add foo
 # > docker run -it -p 26657:26657 -p 26656:26656 -v ~/.simappcli:/root/.simapp simapp simd keys list
 # TODO: demo connecting rest-server (or is this in server now?)
-FROM golang:1.22.1-alpine AS build-env
+FROM golang:1.24-alpine AS build-env
 
 # Install minimum necessary dependencies
 ENV PACKAGES curl make git libc-dev bash gcc linux-headers eudev-dev python3

Makefile
@@ -460,10 +460,10 @@ localnet-build-dlv:
 localnet-build-nodes:
 	$(DOCKER) run --rm -v $(CURDIR)/.testnets:/data cosmossdk/simd \
 			  testnet init-files --v 4 -o /data --starting-ip-address 192.168.10.2 --keyring-backend=test
-	docker-compose up -d
+	docker compose up -d
 
 localnet-stop:
-	docker-compose down
+	docker compose down
 
 # localnet-start will run a 4-node testnet locally. The nodes are
 # based off the docker images in: ./contrib/images/simd-env

contrib/images/simd-dlv/Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.22.1-alpine AS build
+FROM golang:1.24-alpine AS build
 
 RUN apk add build-base git linux-headers libc-dev
 RUN go install github.com/go-delve/delve/cmd/dlv@latest

contrib/images/simd-env/Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.22.1-alpine AS build
+FROM golang:1.24-alpine AS build
 
 RUN apk add build-base git linux-headers
 

server/rosetta/converter.go
@@ -338,8 +338,8 @@ func sdkEventToBalanceOperations(status string, event abci.Event) (operations []
 	default:
 		return nil, false
 	case banktypes.EventTypeCoinSpent:
-		spender := sdk.MustAccAddressFromBech32(string(event.Attributes[0].Value))
-		coins, err := sdk.ParseCoinsNormalized(string(event.Attributes[1].Value))
+		spender := sdk.MustAccAddressFromBech32(event.Attributes[0].Value)
+		coins, err := sdk.ParseCoinsNormalized(event.Attributes[1].Value)
 		if err != nil {
 			panic(err)
 		}
@@ -349,8 +349,8 @@ func sdkEventToBalanceOperations(status string, event abci.Event) (operations []
 		accountIdentifier = spender.String()
 
 	case banktypes.EventTypeCoinReceived:
-		receiver := sdk.MustAccAddressFromBech32(string(event.Attributes[0].Value))
-		coins, err := sdk.ParseCoinsNormalized(string(event.Attributes[1].Value))
+		receiver := sdk.MustAccAddressFromBech32(event.Attributes[0].Value)
+		coins, err := sdk.ParseCoinsNormalized(event.Attributes[1].Value)
 		if err != nil {
 			panic(err)
 		}
@@ -362,7 +362,7 @@ func sdkEventToBalanceOperations(status string, event abci.Event) (operations []
 	// rosetta does not have the concept of burning coins, so we need to mock
 	// the burn as a send to an address that cannot be resolved to anything
 	case banktypes.EventTypeCoinBurn:
-		coins, err := sdk.ParseCoinsNormalized(string(event.Attributes[1].Value))
+		coins, err := sdk.ParseCoinsNormalized(event.Attributes[1].Value)
 		if err != nil {
 			panic(err)
 		}

types/events.go
@@ -130,7 +130,7 @@ func ParseTypedEvent(event abci.Event) (proto.Message, error) {
 
 	attrMap := make(map[string]json.RawMessage)
 	for _, attr := range event.Attributes {
-		attrMap[string(attr.Key)] = json.RawMessage(attr.Value)
+		attrMap[attr.Key] = json.RawMessage(attr.Value)
 	}
 
 	attrBytes, err := json.Marshal(attrMap)
@@ -187,17 +187,6 @@ func (a Attribute) String() string {
 // ToKVPair converts an Attribute object into a Tendermint key/value pair.
 func (a Attribute) ToKVPair() abci.EventAttribute {
 	return abci.EventAttribute{Key: a.Key, Value: a.Value}
-}
-
-func toBytes(i interface{}) []byte {
-	switch x := i.(type) {
-	case []uint8:
-		return x
-	case string:
-		return []byte(x)
-	default:
-		panic(i)
-	}
 }
 
 // AppendAttributes adds one or more attributes to an Event.
@@ -295,7 +284,7 @@ func StringifyEvent(e abci.Event) StringEvent {
 	for _, attr := range e.Attributes {
 		res.Attributes = append(
 			res.Attributes,
-			Attribute{Key: string(attr.Key), Value: string(attr.Value)},
+			Attribute{Key: attr.Key, Value: attr.Value},
 		)
 	}
 